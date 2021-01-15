7ed0499d35f27eb8891120b6f09fdbdae3e3e2ce
oleg <gusevoleg96@gmail.ru>
2021-01-15 12:31:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+
+<div style="position: absolute; top: 0px; right: 0px;">
+    <img width="200" height="200" src="https://redislabs.com/wp-content/uploads/2020/12/RedisLabs_Illustration_HomepageHero_v4.svg">
+</div>
+
+<div style="height: 150px"></div>
+
+# Spring(java) Redis Example
+
+Show how the redis works with Spring(java).
+
+
+# Redis rate-limiting example (front)
+
+![alt text](docs/screenshot001.png)
+
+# Redis rate-limiting example (command line)
+
+![alt text](docs/redis-comand.png)
+
+
+## How to run it locally?
+
+### Run docker compose or install redis manually
+
+Install docker (on mac: https://docs.docker.com/docker-for-mac/install/)
+
+```sh
+docker network create global
+docker-compose up -d --build
+```
+
+#### If you install redis manually open src/main/resources/ folder and provide the values for environment variables in application.properties
+    uri=
+
+#### Run backend
+
+Install gradle (on mac: https://gradle.org/install/)
+
+
+Install JDK (on mac: https://docs.oracle.com/javase/10/install/installation-jdk-and-jre-macos.htm)
+
+``` sh
+gradle wrapper
+./gradlew build
+./gradlew run
+```
---
+++ src/main/java/com/redis/rediscachingjava/controller/Repository.java
@@ -0,0 +1,79 @@
+package com.redis.rediscachingjava.controller;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import redis.clients.jedis.Jedis;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DecimalFormat;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@Service
+public class Repository {
+    @Value("${uri:redis://localhost:6379}")
+    private String uri;
+    Jedis jedis;
+
+
+    @RequestMapping(value = "/repos/{gitName}", produces = { "text/html; charset=utf-8" })
+    @ResponseBody
+    public String getGitData(HttpServletResponse response,
+                             @PathVariable("gitName") String gitName) {
+        if (jedis == null) {
+            jedis = new Jedis(uri);
+        }
+        long startTime = System.nanoTime();
+        String gitData = jedis.get(gitName);
+        boolean isCached = true;
+        if (gitData == null) {
+            gitData = getGitReposData(gitName);
+            isCached = false;
+        }
+
+        response.addHeader("X-Response-Time", getResponseTime(System.nanoTime() - startTime, 1_000_000) );
+        response.addHeader("Access-Control-Expose-Headers", "X-Response-Time");
+        return String.format("{\"username\":\"%s\",\"repos\":\"%s\",\"cached\":%s}", gitName, gitData, isCached);
+    }
+
+    public static String getResponseTime(long num, double divisor) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        return df.format(num / divisor) + "ms";
+    }
+
+
+    private String getGitReposData(String gitName) {
+        try {
+            String sURL = String.format("https://api.github.com/users/%s", gitName);
+            URL url = new URL(sURL);
+            URLConnection request = url.openConnection();
+            request.connect();
+            JsonParser jsonParser = new JsonParser();
+            JsonElement jsonElement = jsonParser.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            try {
+                String gitData = jsonObject.get("public_repos").getAsString();
+                jedis.setex(gitName, 3600, gitData);
+                return gitData;
+            } catch (Exception e){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+}
